e6e49faf53de514e757a3cc26bf1f1ed6457d852
lalitaphalak <lalita@edcast.com>
2015-09-15 22:41:22 +0530

    CMA-8762 Input textarea and checkbox

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+	$(".mdl").on('focusin', ".mdl-input", function() {
+		$(this).parent().addClass('focused');
+		if (!this.value) {
+			$(this).parent().addClass('empty');
+		}
+	});
+
+	$(".mdl").on('focusout', ".mdl-input", function() {
+		$(this).parent().removeClass('focused');
+		if (this.value) {
+			$(this).parent().removeClass('empty');
+		}
+		else {
+			$(this).parent().addClass('empty');
+		}
+	});
+
+	$('.mdl-checkbox-label').on('click', function() {
+  		// For ripple animation
+	    var el = $(this).children('span:first-child');
+	    el.addClass('circle');
+  		var newone = el.clone(true);  
+  		el.before(newone);  
+	  	$(this).children(".circle:last").remove();
+	  	
+	  	// For checked styling change
+		if (!$(this).hasClass('checked')) {
+			$(this).addClass('checked');
+		}
+		else {
+			$(this).removeClass('checked');
+		}
+	}); 
+});
